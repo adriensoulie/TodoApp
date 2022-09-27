3f2b49731c0ea235bad0e29a26a769defe419b25
adriensoulie <soulieadrien1@gmail.com>
2022-09-27 16:37:29 +0700

    [feat] display redux todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
-
+import { RootState } from "./store/store";
 import "./App.css";
 import Todo from "./components/Todo";
-import { useDispatch } from "react-redux";
-import { fetchAllTodos } from "./reducers/todos/todoSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllTodos, selectTodos } from "./reducers/todos/todoSlice";
 import { AppDispatch } from "./store/store";
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
+  const todos = useSelector((state: RootState) => state.todos.todos);
+  console.log(todos);
+
   useEffect(() => {
     dispatch(fetchAllTodos());
   }, [dispatch]);
@@ -15,7 +18,11 @@ function App() {
   return (
     <div className="App">
       TODO App
-      <Todo />
+      <div>
+        {todos.map((todo) => {
+          return <p>{todo.title}</p>;
+        })}
+      </div>
     </div>
   );
 }
